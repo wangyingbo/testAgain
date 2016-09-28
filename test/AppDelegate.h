eea3235c4cda583897a925fcf5bb1714635913d9
wangyingbo <wangyingbo528@126.com>
2016-09-28 17:06:44 +0800

    first

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 王迎博 on 16/9/28.
+//  Copyright © 2016年 王迎博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 王迎博 on 16/9/28.
+//  Copyright © 2016年 王迎博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
